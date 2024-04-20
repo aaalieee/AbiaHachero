04de9bc997172becbbcf77be0547c0e464c7579e
nsein <abianathalie30@gmail.com>
2024-04-20 02:59:38 -0700

    Modify templates

--- psusenv/PSUSphere/projectsite/projectsite/urls.py
+++ psusenv/PSUSphere/projectsite/projectsite/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
-
 ]
