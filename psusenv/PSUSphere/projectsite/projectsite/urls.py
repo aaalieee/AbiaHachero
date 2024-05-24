ddf6ca169751c05aa59d6da6c017e82dec2d4b2b
nsein <abianathalie30@gmail.com>
2024-05-23 20:39:10 -0700

    modify base.html

--- psusenv/PSUSphere/projectsite/projectsite/urls.py
+++ psusenv/PSUSphere/projectsite/projectsite/urls.py
@@ -20,13 +20,11 @@ from django.contrib.auth import views as auth_views
 from studentorg.views import HomePageView, OrganizationList, OrganizationCreateView, OrganizationUpdateView, OrganizationDeleteView, OrgMemberList, OrgMemberCreateView, OrgMemberUpdateView, OrgMemberDeleteView, StudentList, StudentCreateView, StudentUpdateView, StudentDeleteView
 from studentorg.views import CollegeListView, CollegeCreateView, CollegeUpdateView, CollegeDeleteView, ProgramListView, ProgramCreateView, ProgramUpdateView, ProgramDeleteView
 from studentorg import views
-from fire.views import HomePageView, ChartView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , views.HomePageView.as_view(), name='home'),
-    path('dashboard_chart', ChartView.as_view(), name='dashboard-chart'),
     path('organization_list/' , OrganizationList.as_view(), name= 'organization-list'),
     path('organization_list/add/' , OrganizationCreateView.as_view(), name= 'organization-add'),
     path('organization_list/<pk>/' , OrganizationUpdateView.as_view(), name= 'organization-update'),
--- psusenv/PSUSphere/projectsite/studentorg/views.py
+++ psusenv/PSUSphere/projectsite/studentorg/views.py
@@ -10,21 +10,13 @@ from typing import Any
 from django.db.models.query import QuerySet
 from django.db.models import Q
 
+
 @method_decorator(login_required, name='dispatch')
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
     template_name = "home.html"
 
-class ChartView(ListView):
-    template_name = 'chart.html'
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
-    def get_queryset(self, *args, **kwargs):
-        pass
 
 #ORGANIZATION
 class OrganizationList(ListView):
